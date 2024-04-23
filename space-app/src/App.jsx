385466a15dece7d11053bc49337bf4eb821dc34b
Reinaldo Cicarone <reinaldojunior165@gmail.com>
2024-04-23 19:44:52 -0300

    Criacao do cabecalho, menu e  banner

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { styled } from "styled-components"
+import StyleGlobal from './components/StyleGlobal'
+import Cabecalho from "./components/Cabecalho"
+import BarraLateral from "./components/BarraLateral"
+import Banner from "./components/Banner"
+import bannerBackground from "./assets/banner.png"
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Space App
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <FundoGradiente>
+      <StyleGlobal />
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <ConteudoGaleria>
+            <Banner texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={bannerBackground} />
+          </ConteudoGaleria>
+        </MainContainer>
+      </AppContainer>
+    </FundoGradiente>
   )
 }
 
